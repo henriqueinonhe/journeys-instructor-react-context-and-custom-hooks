bbab5b2c4706201a6168a179a18ae154014ec89b
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-03-04 23:19:15 -0300

    Implement format number and date

--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -10,8 +10,15 @@ export const useTranslation = () => {
   }
 
   const t = (key) => translate(language, key);
+  const formatNumber = Intl.NumberFormat(language, {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  }).format;
+  const formatDate = Intl.DateTimeFormat(language).format;
 
   return {
     t,
+    formatNumber,
+    formatDate,
   };
 };
